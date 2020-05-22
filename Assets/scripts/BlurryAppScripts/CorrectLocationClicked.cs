19213f5d41b73aa951443d4b02f57df04a1c46b5
InSaine2k13 <mohamed.h@live.nl>
2020-05-22 23:17:29 +0200

    BlurryApp 0.1

---
+++ Assets/scripts/BlurryAppScripts/CorrectLocationClicked.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectLocationClicked : MonoBehaviour
+{
+    public Sprite[] spriteArray;
+    public SpriteRenderer spriteRenderer;
+
+    public void ChangeSprite()
+    {
+        spriteRenderer.sprite = spriteArray[4];
+        Debug.Log("Button Clicked");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+
+    }
+}
