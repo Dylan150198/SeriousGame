b237c3dc9876ba16e38a6d8d6e148739941d7f1c
Dylan <djh.tenbohmer@student.avans.nl>
2020-05-26 11:12:04 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Minigame - Motor Skills/Scripts/SpawnPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject circle;
+    public bool active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var pieceRotation = Quaternion.AngleAxis(90, Vector3.right);
+        var instantiatedCircle = Instantiate(circle, transform.position, pieceRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RandomSpawnPointPicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnPointPicker : MonoBehaviour
+{
+    public GameObject[] spawnpoints;
+    public GameObject circle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, spawnpoints.Length);
+        var spawnPoint = spawnpoints[rand].transform.position;
+        var pieceRotation = Quaternion.AngleAxis(90, Vector3.right);
+        Instantiate(circle, new Vector3(spawnPoint.x, spawnPoint.y, spawnPoint.z), pieceRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
