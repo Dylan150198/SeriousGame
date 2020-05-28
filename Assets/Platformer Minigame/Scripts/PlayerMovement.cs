f5e8cfeeb71d26e57c71bfe26271b81607ab8667
Ricky <ricky.vandenberg@hotmail.nl>
2020-05-28 14:10:10 +0200

    merge

---
+++ Assets/Platformer Minigame/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public CharacterController2D controller;
+	public Animator animator;
+	public float runSpeed = 40f;
+	private float horizontalMove = 0f;
+	private bool jump = false;
+	public Joystick joystick;
+	
+
+
+
+	// Update is called once per frame
+	private void Update()
+    {
+
+		if (joystick.Horizontal >= .3f)
+		{
+			horizontalMove = runSpeed;
+		}
+		else if (joystick.Horizontal <= -.3f)
+		{
+			horizontalMove = -runSpeed;
+		}
+		else
+		{
+			horizontalMove = 0f;
+		}
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		float verticalMove = joystick.Vertical;
+
+
+		if (verticalMove >= .8f && !animator.GetBool("IsJumping"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+
+	}
+
+	public void OnLanding() {
+		animator.SetBool("IsJumping", false);
+
+		
+	}
+
+	private void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+}
+
