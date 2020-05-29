919ddade1c0bc82fc2fa8c60aa3686460e1ccbd1
nkarimi00 <nadiakarimi206@gmail.com>
2020-05-29 02:43:59 +0200

    Added Moving light

---
+++ Assets/Platformer Minigame/Scripts/JoystickLight.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickLight : MonoBehaviour
+{
+
+	public Transform light;
+	public float speed = 4.0f;
+	private bool touchStart = false;
+	private Vector2 pointA;
+	private Vector2 pointB;
+	
+
+
+	public Transform circle;
+	//public Transform outercircle;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0)) {
+			pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+			circle.transform.position = pointA * -1;
+			//outercircle.transform.position = pointA * -1;
+			circle.GetComponent<SpriteRenderer>().enabled = true;
+			//outercircle.GetComponent<SpriteRenderer>().enabled = true;
+		}
+		if (Input.GetMouseButton(0))
+		{
+			touchStart = true;
+			pointB = pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+		}
+		else {
+			touchStart = false;
+		}
+    }
+
+	private void FixedUpdate()
+	{
+		if (touchStart)
+		{
+			Vector2 offset = pointB - pointA;
+			Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+			MoveLight(direction * -1);
+
+			circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y) * -1;
+		}
+		else {
+			circle.GetComponent<SpriteRenderer>().enabled = false;
+			//outercircle.GetComponent<SpriteRenderer>().enabled = false;
+		}
+	}
+
+	void MoveLight(Vector2 direction) {
+		light.Translate(direction * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Platformer Minigame/Scripts/LightController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LightController : MonoBehaviour
+{
+	
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
+	private Rigidbody2D m_Rigidbody2D;
+	
+	private Vector3 m_Velocity = Vector3.zero;
+
+	[Header("Events")]
+	[Space]
+
+	public UnityEvent OnLandEvent;
+
+	[System.Serializable]
+	public class BoolEvent : UnityEvent<bool> { }
+
+
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+	}
+
+	
+
+
+	public void Move(float move, float v_move)
+	{
+
+			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+			// And then smoothing it out and applying it to the light
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+			Vector3 targetVelocityVertical = new Vector2(m_Rigidbody2D.velocity.x, v_move * 10f);
+			// And then smoothing it out and applying it to the light
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocityVertical, ref m_Velocity, m_MovementSmoothing);
+		
+
+
+
+	}
+
+
+}
