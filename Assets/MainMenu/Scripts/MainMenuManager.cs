5220c1705fb290af3ec4f466a1d140239ada3fe5
InSaine2k13 <mohamed.h@live.nl>
2020-05-31 13:22:49 +0200

    Small bug fix

--- Assets/MainMenu/Scripts/MainMenuManager.cs
+++ Assets/MainMenu/Scripts/MainMenuManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -24,6 +25,7 @@ public class MainMenuManager : MonoBehaviour
         yield return new WaitForSeconds(sceneLoadTime);
         SceneManager.LoadScene(sceneName);
     }
+
 
     public void StartMotoringMaze()
     {
