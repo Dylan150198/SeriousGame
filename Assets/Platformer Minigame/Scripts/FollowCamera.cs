c724c556f0a554e6fd6e1cd4fc0dc510b0f6f3ab
Nadia <nkarimi@student.avans.nl>
2020-06-07 00:23:25 +0200

    level design and camera fix

--- Assets/Platformer Minigame/Scripts/FollowCamera.cs
+++ Assets/Platformer Minigame/Scripts/FollowCamera.cs
@@ -6,46 +6,52 @@ public class FollowCamera : MonoBehaviour
 {
 
 	public Cinemachine.CinemachineVirtualCamera camera;
-	private EdgeCollider2D edgeCollider2D;
-	private float cameraMaxX;
-	private float cameraMaxY;
-	private float cameraMinX;
-	private float cameraMinY;
-
+	GameObject CameraBounds;
 
 	// Start is called before the first frame update
 	void Start()
     {
-		Bounds bounds = OrthographicBounds(camera);
-		edgeCollider2D = GetComponent<EdgeCollider2D>();
-		cameraMaxX = bounds.max.x;
-		cameraMaxY = bounds.max.y;
-		cameraMinX = bounds.min.x;
-		cameraMinY = bounds.min.y;
-
-		Vector2[] points = edgeCollider2D.points;
-		points[0] = new Vector2(cameraMinX, cameraMaxY);
-		points[1] = new Vector2(cameraMaxX, cameraMaxY);
-		points[2] = new Vector2(cameraMaxX, cameraMinY);
-		points[3] = new Vector2(cameraMinX, cameraMinY);
-		points[4] = new Vector2(cameraMinX, cameraMaxY);
-
-		edgeCollider2D.points = points;
+		CameraBounds = new GameObject();
+		GenerateCollidersAcrossScreen();
+	}
+	void GenerateCollidersAcrossScreen()
+	{
 		
+		Vector2 lDCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0f, camera.m_Lens.NearClipPlane));
+		Vector2 rUCorner = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, camera.m_Lens.NearClipPlane));
+		Vector2[] colliderpoints;
+
+		
+		CameraBounds.layer = 11;
+
+		EdgeCollider2D upperEdge = CameraBounds.AddComponent<EdgeCollider2D>();
+		colliderpoints = upperEdge.points;
+		colliderpoints[0] = new Vector2(lDCorner.x, rUCorner.y);
+		colliderpoints[1] = new Vector2(rUCorner.x, rUCorner.y);
+		upperEdge.points = colliderpoints;
+
+		EdgeCollider2D lowerEdge = CameraBounds.AddComponent<EdgeCollider2D>();
+		colliderpoints = lowerEdge.points;
+		colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
+		colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+		lowerEdge.points = colliderpoints;
+
+		EdgeCollider2D leftEdge = CameraBounds.AddComponent<EdgeCollider2D>();
+		colliderpoints = leftEdge.points;
+		colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
+		colliderpoints[1] = new Vector2(lDCorner.x, rUCorner.y);
+		leftEdge.points = colliderpoints;
+
+		EdgeCollider2D rightEdge = CameraBounds.AddComponent<EdgeCollider2D>();
+
+		colliderpoints = rightEdge.points;
+		colliderpoints[0] = new Vector2(rUCorner.x, rUCorner.y);
+		colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+		rightEdge.points = colliderpoints;
 	}
 
-	public Bounds OrthographicBounds(Cinemachine.CinemachineVirtualCamera camera)
+	private void Update()
 	{
-		float screenAspect = camera.m_Lens.Aspect * 2;
-		float cameraHeight = camera.m_Lens.OrthographicSize * 2;
-		Bounds bounds = new Bounds(
-			camera.transform.position,
-			new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
-		return bounds;
-	}
-
-	private void LateUpdate()
-	{
-		edgeCollider2D.transform.position = camera.transform.position;
+		CameraBounds.transform.position = Camera.main.transform.position + new Vector3(5.55f, 1.15f, 0);
 	}
 }
